7c53cec6c8bdd4bcf1315b403b49c968d55fcdc9
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-25 14:43:33 -0600

    Initial commit

---
+++ globalVars/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblShovels = New System.Windows.Forms.Label()
+        Me.txtShovels = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblShovels
+        '
+        Me.lblShovels.AutoSize = True
+        Me.lblShovels.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblShovels.Location = New System.Drawing.Point(14, 91)
+        Me.lblShovels.Name = "lblShovels"
+        Me.lblShovels.Size = New System.Drawing.Size(82, 24)
+        Me.lblShovels.TabIndex = 0
+        Me.lblShovels.Text = "Shovels:"
+        '
+        'txtShovels
+        '
+        Me.txtShovels.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtShovels.Location = New System.Drawing.Point(102, 88)
+        Me.txtShovels.Name = "txtShovels"
+        Me.txtShovels.Size = New System.Drawing.Size(100, 29)
+        Me.txtShovels.TabIndex = 1
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(14, 206)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(150, 33)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "Add to Order"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(14, 275)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(143, 24)
+        Me.lblTotal.TabIndex = 3
+        Me.lblTotal.Text = "Total Shovels: 0"
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(14, 392)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(82, 30)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(173, 31)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Shovel Order"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(458, 453)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtShovels)
+        Me.Controls.Add(Me.lblShovels)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblShovels As System.Windows.Forms.Label
+    Friend WithEvents txtShovels As System.Windows.Forms.TextBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ globalVars/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Dim intTotal As Integer = 0
+    'Global variable
+    'Its "scope" is the entire program
+    'scope = area in which it exists
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        '1) Variables
+        Dim intShovels As Integer
+        'Dim intTotal As Integer = 0
+
+        '2) Set Values
+        intShovels = Val(txtShovels.Text)
+
+        '3) Calculate
+        intTotal = intShovels + intTotal
+
+
+        '4) Display
+        lblTotal.Text = "Total Shovels: " & intTotal
+
+    End Sub ' Destroys all local Variables
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        intTotal = 0
+        lblTotal.Text = "Total Shovels: " & intTotal
+        txtShovels.Text = "" 'Blank Text
+    End Sub
+End Class ' Destroys all other global variables
+' If the variables are not destroyed: memory leak
